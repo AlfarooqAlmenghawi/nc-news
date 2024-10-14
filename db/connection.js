f44ed2f610b15e2386fbe7a040ebce086ec5c0c7
Alfarooq Almenghawi <a.almenghawi123@gmail.com>
2024-10-14 12:31:24 +0100

    done api/topics

--- db/connection.js
+++ db/connection.js
@@ -1,12 +1,18 @@
-const { Pool } = require('pg');
-const ENV = process.env.NODE_ENV || 'development';
+const { Pool } = require("pg");
+const ENV = process.env.NODE_ENV || "development";
 
-require('dotenv').config({
+const path = `${__dirname}/../.env.${ENV}`;
+
+require("dotenv").config({
   path: `${__dirname}/../.env.${ENV}`,
 });
 
+console.log(`the environment is ${ENV}`);
+console.log(`the path is ${path}`);
+console.log(`the database is ${process.env.PGDATABASE}`);
+
 if (!process.env.PGDATABASE) {
-  throw new Error('PGDATABASE not set');
+  throw new Error("PGDATABASE not set");
 }
 
 module.exports = new Pool();
