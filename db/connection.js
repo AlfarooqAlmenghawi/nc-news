6c74817a37b589162859635dcb168812cf67e8c5
Alfarooq Almenghawi <a.almenghawi123@gmail.com>
2024-10-17 14:37:13 +0100

    Added configurations for a PRODUCTION database

--- db/connection.js
+++ db/connection.js
@@ -7,12 +7,19 @@ require("dotenv").config({
   path: `${__dirname}/../.env.${ENV}`,
 });
 
+const config = {};
+
+if (ENV === "production") {
+  config.connectionString = process.env.DATABASE_URL;
+  config.max = 2;
+}
+
 console.log(`the environment is ${ENV}`);
 console.log(`the path is ${path}`);
 console.log(`the database is ${process.env.PGDATABASE}`);
 
-if (!process.env.PGDATABASE) {
-  throw new Error("PGDATABASE not set");
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  throw new Error("PGDATABASE or DATABASE_URL not set");
 }
 
-module.exports = new Pool();
+module.exports = new Pool(config);
