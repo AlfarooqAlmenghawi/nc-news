3cedbb90d8060425ac49ea052edd98c886266ffa
Alfarooq Almenghawi <a.almenghawi123@gmail.com>
2024-10-17 22:54:01 +0100

    nodemon console.log error

--- appfornodemon.js
+++ appfornodemon.js
@@ -73,7 +73,7 @@ app.listen(8000, (err) => {
   if (err) {
     console.log(err);
   } else {
-    console.log("Listening on local server 4000...");
+    console.log("Listening on local server 8000...");
   }
 });
 
