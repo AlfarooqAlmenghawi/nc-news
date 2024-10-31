45b38be9ffc4b2c7b05dbbc4edf42e1b6546c7f0
Alfarooq Almenghawi <a.almenghawi123@gmail.com>
2024-10-31 08:23:33 +0000

    Spinning api back up on render

--- MVC/models/articles.model.js
+++ MVC/models/articles.model.js
@@ -11,16 +11,16 @@ const fetchArticlesAndTotalComments = (queries) => {
   let queryString = `SELECT articles.article_id, articles.title, articles.topic, articles.author, articles.created_at, articles.votes, articles.article_img_url, COUNT(comments.comment_id) AS comment_count FROM articles 
   LEFT JOIN comments ON comments.article_id = articles.article_id`;
 
-  // let validQueries = [
-  //   "article_id",
-  //   "title",
-  //   "topic",
-  //   "author",
-  //   "created_at",
-  //   "votes",
-  //   "article_img_url",
-  //   "comment_count",
-  // ];
+  let validQueries = [
+    "article_id",
+    "title",
+    "topic",
+    "author",
+    "created_at",
+    "votes",
+    "article_img_url",
+    "comment_count",
+  ];
 
   let validOrderBys = ["asc", "desc"];
 
